09dce9dcd2516b8ec829b9468ecc80914c935a63
Nathan9722 <146355716+Nathan9722@users.noreply.github.com>
2024-02-02 01:39:33 +0100

    Refactor max_integer function to multiple_returns function

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-def max_integer(my_list=[]):
-    if len(my_list) <= 0:
-        return None
-
-    max_value = my_list[0]
-    for num in my_list[1:]:
-        if num > max_value:
-            max_value = num
-    return max_value
+def multiple_returns(sentence):
+    if not sentence:
+        return (0, None)
+    return (len(sentence), sentence[0])
