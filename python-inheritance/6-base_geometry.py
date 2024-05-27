bca1f418c01292a13ab00ed1a9c86ddeaeb270cb
Nathan9722 <146355716+Nathan9722@users.noreply.github.com>
2024-05-27 15:18:16 +0200

    python inheritance

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Module for is_kind_of_class method"""
+
+
+class BaseGeometry:
+    """
+    Class BaseGeometry
+    """
+
+    def area(self):
+        """
+        Method that raises an Exception with message area() is not implemented
+        """
+        raise Exception("area() is not implemented")
+    
