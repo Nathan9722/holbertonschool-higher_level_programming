af97d8b73acdb03d708898b82c2f7541d993754c
Nathan9722 <146355716+Nathan9722@users.noreply.github.com>
2024-05-27 15:04:09 +0200

    chore: Refactor 2-args.py for improved readability and maintainability

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-import sys
+# 2-args.py
+# Brennan D Baraban <375@holbertonschool.com>
 
 if __name__ == "__main__":
-    args = sys.argv[1:]
-    nbArgs = len(args)
+    """Print the number of and list of arguments."""
+    import sys
 
-    if nbArgs == 0:
-        print("{} arguments.".format(nbArgs), end="\n")
-    elif nbArgs == 1:
-        print("{} argument:".format(nbArgs), end="\n")
-    elif nbArgs > 1:
-        print("{} arguments:".format(nbArgs), end="\n")
-
-    for index, value in enumerate(args, 1):
-        print("{}: {}".format(index, value))
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
